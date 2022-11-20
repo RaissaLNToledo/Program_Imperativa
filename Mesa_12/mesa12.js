359964753f814ac8e8de1cdf18661b7488865569
RaissaLNToledo <raissaltoledo@yahoo.com.br>
2022-11-19 21:49:37 -0300

    Exercício feito pelo grupo 3

---
+++ Mesa_12/mesa12.js
@@ -0,0 +1,88 @@
+// Grupo 3
+// Integrantes:
+// Raíssa
+// Robson
+
+
+let encontrarGanhador = function(a, b){
+    let pontosA = 0;
+    let pontosB = 0;
+
+
+    //Verifica quantos pontos cada um tem
+    for (let i = 0; i < a.length; i++){
+        if(a[i] > b[i]){
+            pontosA++;
+        }
+        else if(a[i] < b[i]){
+            pontosB++;
+        }
+    }
+
+    //Verificar quem ganhou
+    if(pontosA > pontosB){
+        let ganhador = 'Primeiro Participante';
+        return ganhador;
+    }
+    else if (pontosA < pontosB){
+        let ganhador = 'Segundo Participante';
+        return ganhador;
+    }
+    else{
+        let ganhador = 'Empate';
+        return ganhador;
+    }
+
+}
+
+const Alicia = [23, 69, 32];
+const Bob = [12, 67, 43]
+
+console.log(`O ganhador é: ${encontrarGanhador(Alicia, Bob)}`)
+
+//Extra
+let digitalHouse = function(a, b){
+    for(let i = 1; i <= 100; i++){
+        if(i % a === 0 && i % b !== 0){
+            console.log("Digital");
+        }
+        else if (i % a !==0 && i % b ===0){
+            console.log("House");
+        }
+        else if (i % a ===0 && i % b ===0){
+            console.log("Digital House");
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+
+console.log('------------------');
+console.log('Exercício Extra - Função Digital House')
+digitalHouse(2, 3);
+
+let somaArray = function(a){
+    let soma = 0;
+    for(let i = 0; i < a.length; i++){
+        soma = soma+a[i];
+    }
+    return soma
+}
+
+let simulaJoin = function(a){
+    let soma = '';
+    a.forEach(item => {
+        soma = soma + item;
+    });
+    return soma
+}
+
+const lista = [5, 100, 200];
+console.log('------------------');
+console.log('Exercicio Extra - Função somaArray');
+console.log(somaArray(lista));
+const texto = ["t", "c", "h", "a", "u"];
+console.log('------------------');
+console.log('Exercicio Extra - Função simulaJoin');
+console.log(simulaJoin(texto));
---
+++ Mesa_12/mesaAula12.js
@@ -0,0 +1,45 @@
+// Mesa aula 12
+let comparacao = function(notasA, notasB){
+    let pontosA = 0;
+    let pontosB = 0;
+    let arrayNotas = [];
+    for(let i = 0; i < notasA.length; i++){
+        if(notasA[i] > notasB[i]){
+            pontosA++;
+        }
+        else if(notasA[i] < notasB[i]){
+            pontosB++;
+        }
+    }
+    arrayNotas.push(pontosA, pontosB);
+    return arrayNotas;
+}
+
+let encontrarGanhador = function(participanteA, participanteB){
+    let notasA = participanteA.notas;
+    let notasB = participanteB.notas;
+    let pontosCompetidores = comparacao(notasA, notasB);
+    
+    if(pontosCompetidores[0]>pontosCompetidores[1]){
+        console.log(`O competidor ${participanteA.nome} com as notas ${notasA} ganhou com um total de ${pontosCompetidores[0]} pontos.`);
+    }
+    else if(pontosCompetidores[0]<pontosCompetidores[1]){
+        console.log(`O competidor ${participanteB.nome} com as notas ${notasB} ganhou com um total de ${pontosCompetidores[1]} pontos.`);
+    }
+    else{
+        console.log(`Houve um empate entre os competidores ${participanteA.nome} e ${participanteB.nome} com notas ${notasA} e ${notasB}, respectivamente com um total de ${pontosCompetidores[0]} ponto(s)`);
+    }
+}
+
+let participantes = [
+    {
+        nome: 'Alice',
+        notas: [53, 82, 46],
+    },
+    {
+        nome: 'Bob',
+        notas: [45, 8, 46],
+    }
+]
+
+encontrarGanhador(participantes[0], participantes[1]);
